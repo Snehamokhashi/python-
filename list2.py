76b36a6e6b171189acdb8e418410f5c3ae87b827
Snehamokhashi <115691839+Snehamokhashi@users.noreply.github.com>
2022-10-13 13:09:24 +0530

    by sneha

---
+++ list2.py
@@ -0,0 +1,31 @@
+#count
+#reverse()
+l = [10,20,30,44,40]
+#print(1[::-1])
+l.reverse()
+print(l)
+
+
+#sort()
+l = [3,2,6,9,7,11]
+l.sort()
+print(l)
+
+#sorted(list)
+l = [3,2,6,9,7,11]
+new_list = sorted(l)
+print(new_list)
+
+#sum(list)
+l = [1,2,3,4,5,6,7,8,9,10]
+summ = sum(l)
+print("the sum of the list are:",summ)
+
+
+#min(list) and max(list)
+l = [45,87,99,105,20,15]
+max_ele = max(l)
+print("the max element in list:",max_ele)
+min_ele = min(l)
+print("the min element in list is:",min_ele)
+
